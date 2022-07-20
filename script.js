b4c8ca4ac7bbdeb7780a1e399ee3116215e43491
Cam Acma <c.avantm92@gmail.com>
2022-07-20 11:04:17 -0500

    Create a function to play 5 rounds in a row, add comments

--- script.js
+++ script.js
@@ -3,24 +3,40 @@ const choices = ['r', 'p', 's'];
 //Two names arrays are used to create the win/lose/tie messages to account for capitalization.
 const names = ['rock', 'paper', 'scissors'];
 const namesCapitalized = ['Rock', 'Paper', 'Scissors'];
+let computerWins = 0;
+let userWins = 0;
 
-             
+//Computer will randomly choose a number between 0-2 as its play
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
+//This function plays one round
 function playRound(computer) {
     let userChoice = prompt("What's your choice?").toLowerCase()[0];
+    
     if (computer === choices.indexOf(userChoice)) {
         return `It's a tie! You both chose ${names[computer]}.`;
     } else if (choices.indexOf(userChoice) - computer === -1 || choices.indexOf(userChoice) - computer === 2) {
+        computerWins++;
         return `You lose! ${namesCapitalized[computer]} beats ${names[choices.indexOf(userChoice)]}.`;
     } else if (choices.indexOf(userChoice) - computer === 1 || choices.indexOf(userChoice) - computer === -2) {
+        userWins++;
         return `You win! ${namesCapitalized[choices.indexOf(userChoice)]} beats ${names[computer]}.`; 
     }
 }
 
+//The full game lasts 5 rounds
+function game() {
+    let i = 5;
+    while (i > 0) {
+        i--;
+        console.log(playRound(getComputerChoice()));
+    }
+    return `Computer won: ${computerWins} times
+    User won: ${userWins} times`;
+
+}
 
 
-
-console.log(playRound(0));
+console.log(game());
