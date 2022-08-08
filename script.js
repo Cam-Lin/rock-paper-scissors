c2d4cf955a827aced3d371de10b4449acf36519c
Cam Acma <c.avantm92@gmail.com>
2022-08-08 17:13:32 -0500

    Generate win/lose message after 5 rounds, restart button & sounds on user selection

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ let computerWins = 0;
 let userWins = 0;
 let userChoice = null;
 let computerChoice = null;
-const resultsContainer = document.querySelector('#results');
+const resultsContainer = document.querySelector('.results');
 const pResults = document.createElement('p');
 const pResults2 = document.createElement('p');
+const restartButton = document.createElement('button')
 const userWinsText = document.getElementById('user-wins');
 const computerWinsText = document.getElementById('computer-wins');
+const audio = new Audio();
+const audioList= ['./sounds/gun.mp3', './sounds/armor.mp3', './sounds/axe.mp3']
+
+
 
 //rock = 0, paper = 1, scissors = 2
 //Substracting the  values we can decide the winner and reduce the number of cases
 function playRound(computer) {
     pResults.classList.add('p-results');
     pResults2.classList.add('p-results');
-
-    //Add if computerWins = 5, show defeat message, button to restart the game
-    //Add if userWins = 5, show victory message, button to restart the game
-
     if (computer === parseInt(userChoice)) {
         setTimeout(showResults, 1600);
         pResults.textContent = 'Tie';
         pResults2.textContent = 'Never admit defeat!'
         resultsContainer.appendChild(pResults);
         resultsContainer.appendChild(pResults2);
-        setTimeout(hideResults, 3000);
+        setTimeout(hideResults, 3300);
         console.log('tie');
     } else if (parseInt(userChoice) - computer === -1 || parseInt(userChoice) - computer === 2) {
         computerWins++;
@@ -32,7 +33,9 @@ function playRound(computer) {
         pResults2.textContent = 'Defeat is heresy!'
         resultsContainer.appendChild(pResults);
         resultsContainer.appendChild(pResults2);
-        setTimeout(hideResults, 3000);
+        if (userWins < 5 || computerWins < 5) {
+            setTimeout(hideResults, 3300);
+        }
         console.log('compWins');
     } else if (parseInt(userChoice) - computer === 1 || parseInt(userChoice) - computer === -2) {
         userWins++;
@@ -41,10 +44,51 @@ function playRound(computer) {
         pResults2.textContent = 'Glory to mankind!'
         resultsContainer.appendChild(pResults);
         resultsContainer.appendChild(pResults2);
-        setTimeout(hideResults, 3300);
+        if (userWins < 5 || computerWins < 5) {
+            setTimeout(hideResults, 3300);
+        }
         console.log('userWins'); 
     }
     setTimeout(scoreUpdate, 1600);
+    setTimeout(winnerCheck, 3300);
+}
+
+//Winner check
+function winnerCheck() {
+    if (computerWins == 5) {
+        showResults();
+        resultsContainer.classList.add('final-results-lose');
+        pResults.textContent ='Defeat';
+        pResults2.textContent ='So mankind falls';
+        restartButton.textContent = 'Try Again?'
+        resultsContainer.appendChild(pResults);
+        resultsContainer.appendChild(pResults2);
+        resultsContainer.appendChild(restartButton);
+        restartButton.addEventListener('click', reset);
+}
+    if (userWins == 5) {
+        showResults();
+        resultsContainer.classList.add('final-results-win');
+        pResults.textContent ='Victory';
+        pResults2.textContent ='Rise with glory';
+        restartButton.textContent ='Play Again';
+        resultsContainer.appendChild(pResults);
+        resultsContainer.appendChild(pResults2);
+        resultsContainer.appendChild(restartButton);
+        restartButton.addEventListener('click', reset);
+}
+    return;
+}
+
+function reset() {
+    userWins = 0;
+    computerWins = 0;
+    userWinsText.textContent = userWins;
+    computerWinsText.textContent = computerWins;
+    resultsContainer.classList.remove('final-results-win');
+    resultsContainer.classList.remove('final-results-lose');
+    resultsContainer.removeChild(restartButton);
+    hideResults();
 }
 
 //Side menu animation
@@ -66,8 +110,11 @@ playButtons.forEach((item) => {
         function removePlay(){
             item.classList.remove('play');
         }
+        audio.src = audioList[item];
         item.classList.add('play');
         item.addEventListener('transitionend', removePlay);
+        audio.src = audioList[item['id']];
+        audio.play();
         userChoice = item['id'];
         getComputerChoice();
         
@@ -76,6 +123,8 @@ playButtons.forEach((item) => {
 
         function computerPlay() {
             const computerButton = document.getElementById(computerChoice);
+            audio.src = audioList[computerChoice];
+            audio.play();
             computerButton.classList.add('computer-play');
         }
         function computerPlayRemove() {
@@ -83,7 +132,6 @@ playButtons.forEach((item) => {
             computerButton.classList.remove('computer-play');
         }
         playRound(getComputerChoice());
-      
     });
 });
 
@@ -102,6 +150,7 @@ function scoreUpdate() {
     userWinsText.textContent = userWins;
     computerWinsText.textContent = computerWins;
 }
+
 
 //Auxiliary sideMenu() functions
 function menuOpacityUp() {
